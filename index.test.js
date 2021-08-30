4a0129dc906f3c7fb34d62f0e6b348f08fe19f7a
Lucas Daniel <lucasdaniel.geo@gmail.com>
2021-08-30 10:46:18 -0300

    feat: testing with promises instead callbacks

--- index.test.js
+++ index.test.js
@@ -41,15 +41,14 @@ describe('complete()', function () {
 });
 
 describe('saveToFile()', function() {
-    it('should save a single TODO', function(done) {
+    it('should save a single TODO', async function() {
         let todos = new Todos();
         todos.add('save a CSV');
-        todos.saveToFile(err => {
-            assert.strictEqual(fs.existsSync('todos.csv'), true);
-            let expectedFileContents = 'Title,Completed\nsave a CSV,false\n';
-            let content = fs.readFileSync('todos.csv').toString();
-            assert.strictEqual(content, expectedFileContents);
-            done(err);
-        });
+        await todos.saveToFile()
+
+        assert.strictEqual(fs.existsSync('todos.csv'), true);
+        let expectedFileContents = 'Title,Completed\nsave a CSV,false\n';
+        let content = fs.readFileSync('todos.csv').toString();
+        assert.strictEqual(content, expectedFileContents);
     });
-})
+});
